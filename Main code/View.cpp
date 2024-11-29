cdffce496ef5301b9ad33580e4d742bf8ceafcb8
anvn12 <anmega83@gmail.com>
2024-11-29 16:50:16 +0700

    cacbu3

--- Main code/View.cpp
+++ Main code/View.cpp
@@ -110,7 +110,7 @@ int AskContinue()
     return toupper(getch());
 }
 
-//test hkkljlkjjlkjlkjj
+//test hkkljlkjjlkjlkjj wtddddddddffffffffff
 
 // void display(char board[][BOARD_SIZE]) {
 //     for (int i = 0; i < BOARD_SIZE; i++) {
